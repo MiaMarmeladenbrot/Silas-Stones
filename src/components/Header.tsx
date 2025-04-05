37de3f0394d9092264a9a92ed0c058b7f22d8e4b
Ann-Marie Mecklenburg <a.mecklenburg@dsr-digital.com>
2025-04-05 15:26:23 +0200

    add contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,24 @@
+import { Nav } from "./Nav";
+
 export function Header() {
   return (
-    <div className="flex-col flex justify-center items-center text-center pt-30 pb-10 mx-5">
-      <h1 className="text-white max-w-xl mb-10">
-        The Database of Historic Stone Adhesives
-      </h1>
-      <h4 className="text-white max-w-4xl">
-        This site is a growing collection of historical recipes, texts, and
-        references related to stone adhesives - from ancient binding techniques
-        to early modern formulations. The sources range from artist manuals and
-        fabric rolls from medieval cathedrals to invoices and instruction
-        booklets from historic restoration workshops. Whether you're a
-        researcher, a conservator, or simply curious about how people in the
-        past made stones stick, you'll find sources and context to explore this
-        fascinating craft tradition.
-      </h4>
-    </div>
+    <>
+      <Nav />
+      <div className="flex-col flex justify-center items-center text-center pt-20 pb-10 mx-5">
+        <h1 className="text-white max-w-xl mb-10">
+          The Database of Historic Stone Adhesives
+        </h1>
+        <h4 className="text-white max-w-4xl">
+          This site is a growing collection of historical recipes, texts, and
+          references related to stone adhesives - from ancient binding
+          techniques to early modern formulations. The sources range from artist
+          manuals and fabric rolls from medieval cathedrals to invoices and
+          instruction booklets from historic restoration workshops. Whether
+          you're a researcher, a conservator, or simply curious about how people
+          in the past made stones stick, you'll find sources and context to
+          explore this fascinating craft tradition.
+        </h4>
+      </div>
+    </>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+export function Nav() {
+  return (
+    <nav className="flex justify-end px-10 py-5 text-white">
+      <a
+        className="rounded-lg px-4 py-3 cursor-pointer bg-darkSand text-white"
+        href="mailto:silas.m.ploner@uis.no"
+      >
+        Get in touch
+      </a>
+    </nav>
+  );
+}
