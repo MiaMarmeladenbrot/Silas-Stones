444d0ca090e3b00016a3ed739d7d5d9fb85f2c1a
Ann-Marie Mecklenburg <a.mecklenburg@dsr-digital.com>
2025-07-25 16:04:47 +0200

    add lottie animation

--- src/pages/MaintenancePage.tsx
+++ src/pages/MaintenancePage.tsx
@@ -1,9 +1,12 @@
-import { PiFileMagnifyingGlassLight } from "react-icons/pi";
+import Lottie from "lottie-react";
+import searchBook from "../assets/searchBook.json";
 
 export function MaintenancePage() {
   return (
-    <div className="flex-col flex justify-center items-center text-center pt-20 pb-10 mx-5">
-      <PiFileMagnifyingGlassLight className="text-white size-16 mb-6" />
+    <div className="flex-col flex justify-center items-center text-center pt-10 pb-10 mx-5">
+      <div className="size-60">
+        <Lottie animationData={searchBook} loop={true} />
+      </div>
       <h2 className="text-white max-w-xl mb-10">
         Currently busy searching for recipes ...
       </h2>
